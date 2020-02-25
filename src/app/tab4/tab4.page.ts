ecd994e84b5d3137ec4c45d22ff788de31efd2ba
John Doe <viktorsworkmail@gmail.com>
2020-02-25 12:36:58 -0800

    add button 2,3 but need fix 4position and add clicker funciton

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -9,6 +9,10 @@ import { Component, OnInit } from '@angular/core';
 export class Tab4Page implements OnInit {
   oneGray = "chest1_gray.svg"
   oneStatus = 0;
+  twoGray = "chest2_gray.svg"
+  twoStatus = 0;
+  thrGray = "chest3_gray.svg"
+  thrStatus = 0;
   oneClick(){
     
     if(this.oneGray.includes("gray")){
@@ -20,6 +24,26 @@ export class Tab4Page implements OnInit {
       this.oneStatus= 0 ;
     }
 
+  }
+  twoClick(){
+    if(this.twoGray.includes("gray")){
+      this.twoGray="chest2.svg"
+      this.twoStatus = 1
+    }
+    else{
+      this.twoGray="chest2_gray.svg"
+      this.twoStatus= 0 ;
+    }
+  }
+  thrClick(){
+    if(this.thrGray.includes("gray")){
+      this.thrGray="chest3.svg"
+      this.thrStatus = 1
+    }
+    else{
+      this.thrGray="chest3_gray.svg"
+      this.thrStatus= 0 ;
+    }
   }
   constructor() {
     
