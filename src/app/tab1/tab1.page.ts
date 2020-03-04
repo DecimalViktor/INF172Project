ef33f7a1f6cc0b0ddc89816418dec8de80883df3
ys9412 <60271068+ys9412@users.noreply.github.com>
2020-03-03 22:26:57 -0800

    Added Login button
    Added Login button on the main page. When the login button is clicked, it goes to the Medical Info page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-tab1',
-  templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  selector: "app-tab1",
+  templateUrl: "tab1.page.html",
+  styleUrls: ["tab1.page.scss"]
 })
 export class Tab1Page {
   selectGender; // return male/ female
@@ -13,17 +13,13 @@ export class Tab1Page {
   LastName;
 
   constructor() {}
-  ngOninit(){}
-  getGender(){
-    console.log(this.selectGender)
+  ngOninit() {}
+  getGender() {
+    console.log(this.selectGender);
   }
-  getDOB(){
-    console.log(this.dateOfBirth)
+  getDOB() {
+    console.log(this.dateOfBirth);
   }
-  getLoginData(){
-    
-  }
-  nextButton(){
-
-  }
-  }
+  getLoginData() {}
+  nextButton() {}
+}
