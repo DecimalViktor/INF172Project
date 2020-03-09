326184547cf1717a72ae0e59e51d734ede2c0934
ys9412 <60271068+ys9412@users.noreply.github.com>
2020-03-09 01:05:57 -0700

    Switched tab3 and tab 4, added submit button

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,19 +1,27 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AlertController } from "@ionic/angular";
 
 @Component({
-  selector: 'app-tab3',
-  templateUrl: 'tab3.page.html',
-  styleUrls: ['tab3.page.scss']
+  selector: "app-tab3",
+  templateUrl: "tab3.page.html",
+  styleUrls: ["tab3.page.scss"]
 })
 export class Tab3Page {
+  constructor(public alertCtrl: AlertController) {}
+
   selectMed;
 
+  getMed() {}
 
-
-  getMed(){
-    
+  async submitForm() {
+    const alert = await this.alertCtrl.create({
+      header: "Alert",
+      subHeader: "SubTitle",
+      message: "This is an alert message",
+      buttons: ["OK"]
+    });
+    await alert.present();
+    const result = await alert.onDidDismiss();
+    console.log(result);
   }
-
-  constructor() {}
-
 }
