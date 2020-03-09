1867a8f4e6f834e627546bd6d0517f36a41e47b5
ys9412 <60271068+ys9412@users.noreply.github.com>
2020-03-09 01:55:34 -0700

    need to go back to login page after submitting

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,7 @@
-import { Component } from "@angular/core";
+import { Component, ViewChild } from "@angular/core";
 import { AlertController } from "@ionic/angular";
+import { NavController } from "ionic-angular";
+import { Tab1Page } from "../tab1/tab1.page";
 
 @Component({
   selector: "app-tab3",
@@ -8,20 +10,20 @@ import { AlertController } from "@ionic/angular";
 })
 export class Tab3Page {
   constructor(public alertCtrl: AlertController) {}
-
   selectMed;
 
   getMed() {}
 
   async submitForm() {
     const alert = await this.alertCtrl.create({
-      header: "Alert",
-      subHeader: "SubTitle",
-      message: "This is an alert message",
+      header: "Form Submitted",
+      //subHeader: "SubTitle",
+      message: "Your form is successfully submitted",
       buttons: ["OK"]
     });
     await alert.present();
     const result = await alert.onDidDismiss();
     console.log(result);
+    //this.nav.push(Tab1Page);
   }
 }
